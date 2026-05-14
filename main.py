65321473a0fe3f9935c3a6f1e4ef61c6bce8b8ff
ahnexam <ahnexam@gmail.com>
2026-05-14 11:57:53 +0900

    Update  main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 # from dotenv import load_dotenv
-# import os
+import os
 import streamlit as st
 
 # load_dotenv()
-# api_key = os.getenv('OPENAI_API_KEY')
+api_key = os.getenv('OPENAI_API_KEY')
 
 st.title("_AI 시인_ :sunglasses:")
 
