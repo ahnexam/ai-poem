ce64fbf8bd6c32d24daacdae2aab0ca83642518b
ahnexam <ahnexam@gmail.com>
2026-05-14 11:55:27 +0900

    Update  main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 import streamlit as st
 
 # load_dotenv()
